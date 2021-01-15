58ccebec5abaafee8df344629ed0066d5c64895b
LHBThien <yunheemi.troi@gmail.com>
2021-01-15 08:14:08 +0700

    update admin

--- resources/views/admin/layout/menu.blade.php
+++ resources/views/admin/layout/menu.blade.php
@@ -6,38 +6,38 @@
                         </li>
                         
                         <li>
-                            <a href="admin/category/list"><i class="fa fa-bar-chart-o fa-fw"></i>Danh mục<span class="fa arrow"></span></a>
+                            <a href="admin/category/list"><i class="fa fa-bar-chart-o fa-fw"></i> Danh mục<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="admin/category/list">Danh sách</a>
+                                    <a href="admin/category/list"> Danh sách</a>
                                 </li>
                                 <li>
-                                    <a href="admin/category/add">Thêm</a>
+                                    <a href="admin/category/add"> Thêm</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
-                            <a href="admin/food/list"><i class="fa fa-bar-chart-o fa-fw"></i>Thực đơn<span class="fa arrow"></span></a>
+                            <a href="admin/food/list"><i class="fa fa-bar-chart-o fa-fw"></i> Thực đơn<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="admin/food/list">Danh sách</a>
+                                    <a href="admin/food/list"> Danh sách</a>
                                 </li>
                                 <li>
-                                    <a href="admin/food/add">Thêm</a>
+                                    <a href="admin/food/add"> Thêm</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
 
                         <li>
-                            <a href="admin/slide/list"><i class="fa fa-bar-chart-o fa-fw"></i>Slide<span class="fa arrow"></span></a>
+                            <a href="admin/slide/list"><i class="fa fa-bar-chart-o fa-fw"></i> Slide<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="admin/slide/list">Danh sách</a>
+                                    <a href="admin/slide/list"> Danh sách</a>
                                 </li>
                                 <li>
-                                    <a href="admin/slide/add">Thêm</a>
+                                    <a href="admin/slide/add"> Thêm</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
